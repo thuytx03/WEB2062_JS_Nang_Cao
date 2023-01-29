ea8d0d7a375efbaefb2bd4b34705c52ed4f5c79b
thuytx03 <111112338+thuytx03@users.noreply.github.com>
2023-01-29 21:46:01 +0700

    Short Ciruiting

---
+++ 09-Data-Structures-Operators/107.Short Circuiting/script.js
@@ -0,0 +1,75 @@
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italiy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0, // Open 24 hours
+            close: 24,
+        },
+    },
+
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this, this.mainMenu[mainIndex]];
+    },
+
+    orderDelivery: function ({
+        starterIndex = 1,
+        mainIndex = 0,
+        time = '20:00',
+        address,
+    }) {
+        console.log(
+            `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`,
+        );
+    },
+
+    orderPasta: function (ing1, ing2, ing3) {
+        console.log(
+            `Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`,
+        );
+    },
+
+    orderPizza: function (mainIngredient, ...otherIngredients) {
+        console.log(mainIngredient);
+        console.log(otherIngredients);
+    },
+};
+
+console.log('----OR----');
+// Use ANY data type, return ANY data type, short-circuiting
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+console.log(undefined || 0 || '' || 'hello' || 23 || null);
+
+restaurant.numGuests = 23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('----AND----');
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+
+console.log('hello' && 23 && null && 'Jonas');
+
+// Practical example
+if (restaurant.orderPizza) {
+    restaurant.orderPizza('mushrooms', 'spinach');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
