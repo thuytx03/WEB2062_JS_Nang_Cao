17cda022a4921a25a97d3a34c008c3b1c6e0019b
thuytx03 <111112338+thuytx03@users.noreply.github.com>
2023-02-01 22:15:26 +0700

    update

---
+++ 09-Data-Structures-Operators/119.Summary_Which Data Structure to Use/script.js
@@ -0,0 +1,57 @@
+const weekends = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+    [weekends[3]]: {
+        open: 12,
+        close: 22,
+    },
+    [weekends[4]]: {
+        open: 11,
+        close: 23,
+    },
+    [weekends[5]]: {
+        open: 0, // Open 24 hours
+        close: 24,
+    },
+};
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italiy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    order(starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this, this.mainMenu[mainIndex]];
+    },
+
+    // ES6 Enhanced object literals
+    openingHours,
+
+    orderDelivery({
+        starterIndex = 1,
+        mainIndex = 0,
+        time = '20:00',
+        address,
+    }) {
+        console.log(
+            `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`,
+        );
+    },
+
+    orderPasta(ing1, ing2, ing3) {
+        console.log(
+            `Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`,
+        );
+    },
+
+    orderPizza(mainIngredient, ...otherIngredients) {
+        console.log(mainIngredient);
+        console.log(otherIngredients);
+    },
+};
+
+
+
+
