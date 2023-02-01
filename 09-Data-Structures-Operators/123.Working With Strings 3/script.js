3b0736667e54e83eaa178059261ab3d0a5af0329
thuytx03 <111112338+thuytx03@users.noreply.github.com>
2023-02-01 22:29:18 +0700

    Working With Strings

---
+++ 09-Data-Structures-Operators/123.Working With Strings 3/script.js
@@ -0,0 +1,103 @@
+const weekends = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+    [weekends[3]]: {
+        open: 12,
+        close: 22,
+    },
+    [weekends[4]]: {
+        open: 11,
+        close: 23,
+    },
+    [weekends[5]]: {
+        open: 0, // Open 24 hours
+        close: 24,
+    },
+};
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italiy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    order(starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this, this.mainMenu[mainIndex]];
+    },
+
+    // ES6 Enhanced object literals
+    openingHours,
+
+    orderDelivery({
+        starterIndex = 1,
+        mainIndex = 0,
+        time = '20:00',
+        address,
+    }) {
+        console.log(
+            `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`,
+        );
+    },
+
+    orderPasta(ing1, ing2, ing3) {
+        console.log(
+            `Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`,
+        );
+    },
+
+    orderPizza(mainIngredient, ...otherIngredients) {
+        console.log(mainIngredient);
+        console.log(otherIngredients);
+    },
+};
+
+console.log('a+very+nice+string'.split('+'));
+console.log('Jonas Schmedtmann'.split(' '));
+
+const [firstName, lastName] = 'Jonas Schmedtmann'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+};
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('jonas schmedtmann');
+
+// Padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(20, '+').padEnd(30, '+'));
+console.log('Jonas'.padStart(20, '+').padEnd(30, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(64637836));
+console.log(maskCreditCard(43378463864647384));
+console.log(maskCreditCard('334859493847755774747'));
+
+// Repeat
+const message2 = 'Bad waether... All Departues Delayed... ';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'🛩'.repeat(n)}`);
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
+
