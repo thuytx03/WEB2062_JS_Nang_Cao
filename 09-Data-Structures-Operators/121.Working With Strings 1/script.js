9bf840afa889b5d26b5c7fd3f78dea5f0edcb067
thuytx03 <111112338+thuytx03@users.noreply.github.com>
2023-02-01 22:28:04 +0700

    Working With Strings

---
+++ 09-Data-Structures-Operators/121.Working With Strings 1/script.js
@@ -0,0 +1,95 @@
+const weekends = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+    [weekends[3]]: {
+        open: 12,
+        close: 22,
+    },
+    [weekends[4]]: {
+        open: 11,
+        close: 23,
+    },
+    [weekends[5]]: {
+        open: 0, // Open 24 hours
+        close: 24,
+    },
+};
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italiy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    order(starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this, this.mainMenu[mainIndex]];
+    },
+
+    // ES6 Enhanced object literals
+    openingHours,
+
+    orderDelivery({
+        starterIndex = 1,
+        mainIndex = 0,
+        time = '20:00',
+        address,
+    }) {
+        console.log(
+            `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`,
+        );
+    },
+
+    orderPasta(ing1, ing2, ing3) {
+        console.log(
+            `Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`,
+        );
+    },
+
+    orderPizza(mainIngredient, ...otherIngredients) {
+        console.log(mainIngredient);
+        console.log(otherIngredients);
+    },
+};
+
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('B737'[0]);
+
+console.log(airline.length);
+console.log('B737'.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('portugal'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') console.log('You got the middle seat 😬');
+  else console.log('You got lucky 😎');
+};
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+console.log(new String('jonas'));
+console.log(typeof new String('jonas'));
+
+console.log(typeof new String('jonas').slice(1));
+
+
